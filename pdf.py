89ca37c82cc31613460a27b2b52b4b5163ce334c
aryanap11 <104253149+aryanap11@users.noreply.github.com>
2024-11-05 15:56:57 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -124,7 +124,7 @@ def process_pdf(uploaded_file):
 def main_app():
     st.title("PDF Background Cleaner")
     st.write(
-        "Easily transform PDFs with dark backgrounds into print-friendly, high-contrast documents and thank me if it was useful to you through the "Connect with Me" section!")
+        "Easily transform PDFs with dark backgrounds into print-friendly, high-contrast documents and thank me if it was useful to you through the 'Connect with Me' section!")
     uploaded_file = st.file_uploader(
         "Upload your PDF file to begin", type=["pdf"])
 
